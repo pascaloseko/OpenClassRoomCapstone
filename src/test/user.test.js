9aa89e127249d9e63d44fe1e30ae1adc82d5b30c
Pascal <pascal.ondieki21@gmail.com>
2019-11-01 18:42:17 +0300

    test suite pass

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -2,14 +2,32 @@ const expect = require("chai").expect;
 import request from 'supertest';
 import app from '../index';
 
+function makeEmail() { 
+    var strValues="abcdefg12345"; 
+    var strEmail = ""; 
+    var strTmp; 
+    for (var i=0;i<10;i++) { 
+    strTmp = strValues.charAt(Math.round(strValues.length*Math.random())); 
+    strEmail = strEmail + strTmp; 
+    } 
+    strTmp = ""; 
+    strEmail = strEmail + "@"; 
+    for (var j=0;j<8;j++) { 
+    strTmp = strValues.charAt(Math.round(strValues.length*Math.random())); 
+    strEmail = strEmail + strTmp; 
+    } 
+    strEmail = strEmail + ".com" 
+    return strEmail; 
+ } 
+
 describe("POST /", () => {
-    it("should return user when the all request body is valid", async (done) => {
+    it("should return user when the all request body is valid", async () => {
         const res = await request(app)
             .post("/user")
             .send({
                 firstName: "Test8",
                 lastName: "Test",
-                email: "test14@mail.com",
+                email: makeEmail(),
                 password: "test",
                 gender: "Male",
                 jobRole: "Web Dev",
@@ -19,6 +37,5 @@ describe("POST /", () => {
             });
             expect(res.status).to.equal(201);
             expect(res.body).to.have.property("user");
-            done();
     });
 });
